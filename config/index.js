8ec783e79916d908b82af09935daab4015b0f49e
worldlzh2020666 <109351705+worldlzh2020666@users.noreply.github.com>
2022-08-27 23:45:07 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,38 +1,25 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx5c31040462b08504",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "e329383a5b653ac7bf1879122237c8c0",
     // 模板消息id
-    templateId: "",
+    templateId: "TWFiT_-m7j9CT6rAe_Cml2G0TwTPoC3t1gyr6ZeAszE",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: [oqBaO6ax3J6qDe1eKSOor7SBdjU4],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "北京",
     // 所在城市
-    city: "惠州",
-    // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
-    birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
-    ],
-    // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
-    // 结婚纪念日
-    marryDate: "2020-01-04"
-    }
+    city: "北京",
 
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
 // 天气：{{weather.DATA}}  
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
-// {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
